aab5b5f4d36895feb0187457899f1ccaeb250080
Bianca1232023 <150062646+Bianca1232023@users.noreply.github.com>
2025-11-30 18:32:31 -0300

    deletei a tabela opnion e juntei no respondent + criei uma nova tabela film para fazer relação com film_seen e film_ranking e respondent + mudeialgums tipos de colunas

--- Scripts normalização/CSV 1/normalização_DDL.sql
+++ Scripts normalização/CSV 1/normalização_DDL.sql
@@ -1,7 +1,12 @@
+CREATE TABLE film (
+    filmID INT PRIMARY KEY,
+    film_name VARCHAR(100) NOT NULL
+);
+
 CREATE TABLE respondent(
-    respondentID SERIAL PRIMARY KEY,
+    respondentID INT PRIMARY KEY,
     gender VARCHAR(50),
-    age VARCHAR(50),
+    age INT,
     household_income VARCHAR(50),
     education VARCHAR(50),
     region VARCHAR(50),
@@ -9,28 +14,34 @@ CREATE TABLE respondent(
     fan_of_star_wars VARCHAR(50),
     fan_of_startrek VARCHAR(50),
     fan_of_expanded_universe VARCHAR(50),
-    who_shot_first VARCHAR(50)
+    who_shot_first VARCHAR(50),
+    opinion_han VARCHAR(50),
+    opinion_luke VARCHAR(50),
+    opinion_leia VARCHAR(50),
+    opinion_anakin VARCHAR(50),
+    opinion_palpatine VARCHAR(50),
+    opinion_vader VARCHAR(50),
+    opinion_calrissian VARCHAR(50),
+    opinion_boba VARCHAR (50),
+    opinion_C-3P0 VARCHAR (50),
+    opinion_R2 VARCHAR(50),
+    opinion_jarjar VARCHAR(50),
+    opinion_padme VARCHAR (50),
+    opinion_yoda VARCHAR(50)
 );
 
 CREATE TABLE film_seen (
-    id SERIAL PRIMARY KEY,
-    respondent_id INT REFERENCES respondent(respondent_id),
-    film_name VARCHAR(100),
-    seen VARCHAR(50) 
+    film_seen_id INT PRIMARY KEY,
+    respondent_id INT NOT NULL REFERENCES respondent(respondentID),
+    film_id INT NOT NULL REFERENCES film(filmID),
+    seen VARCHAR(50)
 );
 
 CREATE TABLE film_ranking (
-    id SERIAL PRIMARY KEY,
-    respondent_id INT REFERENCES respondent(respondent_id),
-    film_name VARCHAR(100),
+    film_ranking_id INT PRIMARY KEY,
+    respondent_id INT NOT NULL REFERENCES respondent(respondentID),
+    film_id INT NOT NULL REFERENCES film(filmID),
     ranking INT
-);
-
-CREATE TABLE character_opinion (
-    id SERIAL PRIMARY KEY,
-    respondent_id INT REFERENCES respondent(respondent_id),
-    character_name VARCHAR(100),
-    opinion VARCHAR(50)  
 );
 
 
