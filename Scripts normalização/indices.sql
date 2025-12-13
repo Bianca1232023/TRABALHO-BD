0ab0af6a2dfe88f9c1ceed5d1761f905acb2dd76
Your Name <mariaquaresma2003@gmail.com>
2025-12-13 18:38:01 -0300

    refiz os indices depois do novo DDL e DML

--- Scripts normalização/indices.sql
+++ Scripts normalização/indices.sql
@@ -1,35 +1,23 @@
---indice da tabela filme
-CREATE UNIQUE INDEX idx_film_filmid ON film(filmID);
-CREATE INDEX idx_film_film_name ON film(film_name);
+create index idx_age_group_start_end on age_group(age_range_start, age_range_end);
+create index idx_household_income_start_end on household_income(income_range_start, income_range_end);
 
---indice da tabela RespondentID
-CREATE UNIQUE INDEX idx_respondentid_pk ON RespondentID(respondent_id);
+create index idx_respondent_gender on respondent(gender_id);
+create index idx_respondent_age_group on respondent(age_group_id);
+create index idx_respondent_income on respondent(household_income_id);
+create index idx_respondent_education on respondent(education_level_id);
+create index idx_respondent_region on respondent(region_id);
 
---indice da tabela respostas
-CREATE UNIQUE INDEX idx_respostas_id ON respostas(id);
-CREATE INDEX idx_respostas_respondent_id ON respostas(respondent_id);
-CREATE INDEX idx_respostas_age ON respostas(age);
-CREATE INDEX idx_respostas_gender ON respostas(gender);
-CREATE INDEX idx_respostas_region ON respostas(region);
+create index idx_answer_option_question on answer_option(question_id);
 
---indice da tabela filme_seeen
-CREATE UNIQUE INDEX idx_film_seen_id ON film_seen(film_seen_id);
-CREATE INDEX idx_film_seen_respondent_id ON film_seen(respondent_id);
-CREATE INDEX idx_film_seen_film_id ON film_seen(film_id);
-CREATE INDEX idx_film_seen_respondent_film ON film_seen(respondent_id, film_id);
+create index idx_answer_option_id on answer(option_id);
+create index idx_answer_question_id on answer(question_id);
 
---indice da tabela film_ranking
-CREATE UNIQUE INDEX idx_film_ranking_id ON film_ranking(film_ranking_id);
-CREATE INDEX idx_film_ranking_respondent_id ON film_ranking(respondent_id);
-CREATE INDEX idx_film_ranking_film_id ON film_ranking(film_id);
-CREATE INDEX idx_film_ranking_respondent_film ON film_ranking(respondent_id, film_id);
+create index idx_film_seen_respondent on film_seen(respondent_id);
+create index idx_film_seen_film on film_seen(film_id);
 
---indice da tabela character_film
-CREATE UNIQUE INDEX idx_character_film_id ON character_film(character_id);
-CREATE INDEX idx_character_film_name ON character_film(character_name);
+create index idx_film_ranking_respondent on film_ranking(respondent_id);
+create index idx_film_ranking_film on film_ranking(film_id);
 
---indice da tabela character_opnion
-CREATE UNIQUE INDEX idx_character_opinion_id ON character_opinion(character_opinion_id);
-CREATE INDEX idx_character_opinion_respondent_id ON character_opinion(respondent_id);
-CREATE INDEX idx_character_opinion_character_id ON character_opinion(character_id);
-CREATE INDEX idx_character_opinion_resp_char ON character_opinion(respondent_id, character_id);
+create index idx_character_opinion_respondent on character_opinion(respondent_id);
+create index idx_character_opinion_character on character_opinion(character_id);
+create index idx_character_opinion_option on character_opinion(option_id);
